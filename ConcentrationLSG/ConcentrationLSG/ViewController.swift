72db10b71e66ae0630d29db1ba76ddde1ea61d5c
Pablonura91 <pablonura91@gmail.com>
2019-03-07 07:00:34 -0800

    others

--- ConcentrationLSG/ConcentrationLSG/ViewController.swift
+++ ConcentrationLSG/ConcentrationLSG/ViewController.swift
@@ -16,22 +16,26 @@ class ViewController: UIViewController {
     @IBAction func actionButton(_ sender: UIButton) {
         switch sender.tag {
             case 1...16:
-                if (!concentration.cards[sender.tag - 1].isFadeUp){
+                if (concentration.indexOfOneAndOnlyFaceUpCard == nil){
                 
                     concentration.chooseCard(at: sender.tag - 1)
                     
                     outletCollection[sender.tag - 1].setTitle(concentration.cards[sender.tag - 1].emoji, for: UIControl.State.normal)
                     outletCollection[sender.tag - 1	].backgroundColor = UIColor.clear
                     
-                    if (concentration.indexOfOneAndOnlyFaceUpCard == nil){
+                } else {
+                    concentration.chooseCard(at: sender.tag - 1)
+                    
                     for iterator in concentration.cards.indices{
-                        if (!concentration.cards[iterator].isMatched){
-                            outletCollection[iterator].setTitle("", for: UIControl.State.normal)
-                            outletCollection[iterator].backgroundColor = UIColor.gray
+                            if (!concentration.cards[iterator].isMatched){
+                                outletCollection[iterator].setTitle("", for: UIControl.State.normal)
+                                outletCollection[iterator].backgroundColor = UIColor.gray
+                            }
                         }
-                    }
                 }
-            }
+                
+                outletCollection[sender.tag - 1].setTitle(concentration.cards[sender.tag - 1].emoji, for: UIControl.State.normal)
+                outletCollection[sender.tag - 1    ].backgroundColor = UIColor.clear
             default:
                 break
         }
